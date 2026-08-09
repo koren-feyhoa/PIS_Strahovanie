44b1d5661d919783d022f314beeeb3a9a468f4a1
koren_feyhoa <kk9641288@gmail.com>
2026-08-09 13:05:24 +0700

    Редактирование моделей. работа с бд. возвращение к проекту

--- JSON_function.py
+++
@@ -1,9 +0,0 @@
-import json
-from typing import Dict
-
-
-def SerializeJSON(Dictionary:Dict[str,str]):
-    return json.dumps(Dictionary)
-
-def DeserializaJSON(strJSON: str):
-    return json.loads(strJSON)
--- models.py
+++ models.py
@@ -1,14 +1,19 @@
+import enum
 import uuid
 from datetime import datetime
+from tkinter.constants import CASCADE
 from typing import Optional
 
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
-from sqlalchemy import Column, Integer, String, ForeignKey,JSON, DateTime, Float, Date
+from sqlalchemy import Column, Integer, String, ForeignKey,JSON, DateTime, Float, Date, PrimaryKeyConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from database import Base
 
+class RoleEnum(str, enum.Enum):
+    CLIENT="клиент"
+    AGENT="агент"
 
 class Client(Base):
     __tablename__='client'
@@ -27,12 +32,56 @@ class Agent(Base):
     fullname = Column(String)
     email = Column(String(50), unique=True)
     password: Mapped[str]=mapped_column(String(50))
+
 class Profile(Base):
     __tablename__ = 'profile'
     id:Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     client_id:Mapped[int] = mapped_column(ForeignKey('client.id'))
     type_document=Column(String)
     info=Column(JSON)
+
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    role: Mapped[RoleEnum] = mapped_column(default=RoleEnum.CLIENT, nullable=False)
+    name:Mapped[str]=mapped_column(nullable=True)
+    last_name:Mapped[str]=mapped_column(nullable=True)
+    email:Mapped[str]=mapped_column(String(50), unique=True)
+    password: Mapped[str] = mapped_column(String(50))
+    phone: Mapped [str] = mapped_column(String(30), nullable=True)
+
+
+
+#?????????????????????????????
+
+class Document(Base):
+    __tablename__='document'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    client_id:Mapped[int] = mapped_column(ForeignKey('client.id', ondelete=CASCADE))
+    document_type_id:Mapped[int]=mapped_column(ForeignKey('document_type.id',ondelete=CASCADE))
+    created_at:Mapped[Date]=mapped_column(Date)
+    updated_at:Mapped[Date]=mapped_column(Date)
+
+class DocumentType(Base):
+    __tablename__ = 'document_type'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str]=mapped_column(String)
+    description:Mapped[str]=mapped_column(String)
+
+class InsuranceType(Base):
+    __tablename__ = 'insurance_type'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+
+
+class RequiredDocuments(Base):
+    __tablename__='required_document'
+    insurance_type_id:Mapped[int]=mapped_column(ForeignKey('insurance_type.id', ondelete=CASCADE),primary_key=True,  nullable=False)
+    document_type_id:Mapped[int]=mapped_column(ForeignKey('document_type.id', ondelete=CASCADE),primary_key=True,  nullable=False)
+    __table_args__=(
+        PrimaryKeyConstraint('insurance_type_id', 'document_type_id'),
+    )
 
 class Application(Base):
     __tablename__='application'
@@ -62,3 +111,18 @@ class Contract(Base):
     file_path: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     file_time: Mapped[Optional[DateTime]] = mapped_column(DateTime, nullable=True)
     status_contract: Mapped[str] = mapped_column(String)
+
+class Passport(Base):
+    __tablename__='passport'
+    id: Mapped[int]=mapped_column(primary_key=True)
+    document_id: Mapped[int] = mapped_column(ForeignKey('document.id'))
+    series:Mapped[int]=mapped_column(Integer)
+    number: Mapped[int] = mapped_column(Integer)
+    name:Mapped[str]=mapped_column(String)
+    last_name:Mapped[str]=mapped_column(String)
+    patronymic:Mapped[str]=mapped_column(String)
+    day_of_birth:Mapped[Date]=mapped_column(Date)
+    day_of_get:Mapped[Date]=mapped_column(Date)
+    who_give:Mapped[str]=mapped_column(String)
+
+#Доделать профайл переделать агента и клиента с выделением роли
